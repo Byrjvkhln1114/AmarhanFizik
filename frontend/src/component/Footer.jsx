501588329f294f13ad5a4a08168db56f15eb26de
22HOP2C-Temujin <Temujin@iMac-2021-09s-iMac.local>
2023-03-29 18:55:35 +0800

    tilted

---
+++ frontend/src/component/Footer.jsx
@@ -0,0 +1,12 @@
+
+
+export const Footer = () => {
+    return <div style={{width:"100vw",height:"35vh",backgroundColor:"#181837"}} className="">
+            <div>
+            <img style={{ height: "10vh" }} src="Logo.png" alt="" />
+            <div style={{width:"17vw", height:"10vh", fontSize:"26px", border:"8px"}}  className="d-flex justify-content-center align-items-center text-white border-warning border rounded hover:bg-white">
+                Contact Us
+            </div>
+            </div>
+    </div>
+} 
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,56 +1,60 @@
 import { useState } from "react";
 import { Header } from "../component/header";
+import { Footer } from "../component/Footer";
 export const Home = () => {
   return (
-    <div
-      className="w-100 d-flex flex-column align-items-center text-light  gap-5"
-      style={{ background: "#1f1f47", height: "100vh" }}
-    >
-      <Header></Header>
-      <h1 className="d-flex mt-5">
-        Физик бол асуудал
-        <h1 className="ms-2 " style={{ color: "#f3573c" }}>
-          БИШ !
+    <div>
+      <div
+        className="w-100 d-flex flex-column align-items-center text-light  gap-5"
+        style={{ background: "#1f1f47", height: "90vh" }}
+      >
+        <Header></Header>
+        <h1 className="d-flex mt-5">
+          Физик бол асуудал
+          <h1 className="ms-2 " style={{ color: "#f3573c" }}>
+            БИШ !
+          </h1>
         </h1>
-      </h1>
-      <div className="d-flex w-75 justify-content-between align-items-center ">
-        <div
-          className="d-flex flex-column align-items-center gap-5 justify-content-center"
-          style={{
-            background: "#5381f9",
-            width: "30%",
-            height: "45vh",
-            borderRadius: "30px",
-          }}
-        >
-          <img style={{ height: "60%" }} src="formula.png"></img>
-          <h2>Томьёо</h2>
-        </div>
-        <div
-          className="d-flex flex-column align-items-center gap-5 justify-content-center"
-          style={{
-            background: "#e45a7e",
-            width: "30%",
-            height: "45vh",
-            borderRadius: "30px",
-          }}
-        >
-          <img style={{ height: "60%" }} src="definition.png"></img>
-          <h2>Тайлбар толь</h2>
-        </div>
-        <div
-          className="d-flex flex-column align-items-center gap-5 justify-content-center"
-          style={{
-            background: "#be29f8",
-            width: "30%",
-            height: "45vh",
-            borderRadius: "30px",
-          }}
-        >
-          <img style={{ height: "60%" }} src="circuit.png"></img>
-          <h2>Хэлхээ</h2>
+        <div className="d-flex w-75 justify-content-between align-items-center ">
+          <div
+            className="d-flex flex-column align-items-center gap-5 justify-content-center"
+            style={{
+              background: "#5381f9",
+              width: "30%",
+              height: "45vh",
+              borderRadius: "30px",
+            }}
+          >
+            <img style={{ height: "60%" }} src="formula.png"></img>
+            <h2>Томьёо</h2>
+          </div>
+          <div
+            className="d-flex flex-column align-items-center gap-5 justify-content-center"
+            style={{
+              background: "#e45a7e",
+              width: "30%",
+              height: "45vh",
+              borderRadius: "30px",
+            }}
+          >
+            <img style={{ height: "60%" }} src="definition.png"></img>
+            <h2>Тайлбар толь</h2>
+          </div>
+          <div
+            className="d-flex flex-column align-items-center gap-5 justify-content-center"
+            style={{
+              background: "#be29f8",
+              width: "30%",
+              height: "45vh",
+              borderRadius: "30px",
+            }}
+          >
+            <img style={{ height: "60%" }} src="circuit.png"></img>
+            <h2>Хэлхээ</h2>
+          </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
