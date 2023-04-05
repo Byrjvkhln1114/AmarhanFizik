aeb4eea2ee2025c7eb31a86cf16c3feb2967f20a
Hop2C_Chinbo <hop2c_chinbo@iMac-12s-iMac.local>
2023-04-05 17:35:49 +0800

    login sign up

--- frontend/src/pages/Sign_Up.jsx
+++ frontend/src/pages/Sign_Up.jsx
@@ -1,8 +1,7 @@
 import { Buton } from "../component/Buton";
 import { Header } from "../component/header";
-import { Link } from "react-router-dom";
 
-export const Login = () => {
+export const Sign_Up = () => {
   return (
     <div
       style={{ background: "#1f1f47", height: "100vh" }}
@@ -23,11 +22,26 @@ export const Login = () => {
         <div className="w-75">
           <p style={{ fontSize: "25px", fontWeight: "300" }}>Тавтай морил!</p>
         </div>
-        <p style={{ color: "#1F1F47", fontSize: "50px" }}>Нэвтрэх</p>
+        <p style={{ color: "#1F1F47", fontSize: "50px" }}>Бүртгүүлэх</p>
         <div
           style={{ height: "45vh", width: "20vw" }}
           className=" d-flex flex-column justify-content-around"
         >
+          <div>
+            <h5 style={{ fontWeight: "400" }}>Хэрэглэгчийн нэр</h5>
+            <input
+              style={{
+                border: "1px solid black",
+                outline: "none",
+                width: "20vw",
+                height: "5vh",
+                borderRadius: "5px",
+                paddingLeft: "20px",
+              }}
+              type="text"
+              placeholder="Хэрэглэгчийн нэрээ бичнэ үү"
+            />
+          </div>
           <div>
             <h5 style={{ fontWeight: "400" }}>И-мэйл</h5>
             <input
@@ -43,6 +57,7 @@ export const Login = () => {
               placeholder="И-мэйлээ бичнэ үү"
             />
           </div>
+
           <div>
             <h5 style={{ fontWeight: "400" }}>Нууц үг</h5>
             <input
@@ -57,37 +72,24 @@ export const Login = () => {
               type="password"
               placeholder="Нууц үг бичнэ үү"
             />
-            <div style={{ display: "flex", justifyContent: "flex-end" }}>
-              <Link style={{ textDecoration: "none" }} to="/forgot">
-                <h5
-                  style={{
-                    justifyContent: "end",
-                    color: "#F3573C",
-                    fontWeight: "300",
-                  }}
-                >
-                  Нууц үг мартсан ?
-                </h5>
-              </Link>
-            </div>
+          </div>
+          <div>
+            <h5 style={{ fontWeight: "400" }}>Нууц үг давтна уу</h5>
+            <input
+              style={{
+                border: "1px solid black",
+                outline: "none",
+                width: "20vw",
+                height: "5vh",
+                borderRadius: "5px",
+                paddingLeft: "20px",
+              }}
+              type="password"
+              placeholder="Нууц үг давтна уу"
+            />
           </div>
 
           <Buton></Buton>
-
-          <h5
-            style={{
-              display: "flex",
-              justifyContent: "center",
-              fontSize: "20px",
-              color: "#F3573C",
-            }}
-          >
-            <span style={{ color: "#7D7D7D" }}>Та бүртгэлтэй юу? ㅤ</span>
-            <Link style={{ textDecoration: "none" }} to="/signup">
-              <span style={{ color: "#F3573C" }}>БҮРТГҮҮЛЭХ</span>
-            </Link>
-            ㅤ
-          </h5>
         </div>
       </div>
     </div>
