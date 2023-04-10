07b11889ef6a3d1f4c29149c117dd80a125140aa
22HOP2C-Temujin <Temujin@iMac-2021-09s-iMac.local>
2023-04-10 16:32:23 +0800

    mfooter

--- backend/index.js
+++ backend/index.js
@@ -6,7 +6,7 @@ const app = express();
 const port = 8000;
 app.use(express.json());
 app.use(cors());
-app.use(FormulaRouter);
+app.use(FormulaRouter);3
 const startServer= async ()=>{
   await connectDatabase();
   app.listen(port, () => {
